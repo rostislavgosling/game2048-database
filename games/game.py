c0f1b71c811dbcdeb684d3288439c0e1a0cdab98
biboradnr <biborandnr1@gmail.com>
2023-08-23 13:37:20 +0300

    First commit

---
+++ games/game.py
@@ -0,0 +1,139 @@
+import random
+import numpy as np
+
+
+class Game:
+
+    __commands = ['up', 'down', 'left', 'right']
+
+    def __init__(self, f=np.array([[0, 0, 0, 0] for _ in range(4)])):
+        self.__field = f
+        self.__score = 0
+
+    def __str__(self):
+        return str(self.__field)
+
+    # Function for adding a number to the empty cell in the field
+    def __add_one(self) -> bool:
+        # List for empty cells
+        empty = []
+
+        # Go through all the cells
+        for i, row in enumerate(self.__field):
+            for j, cell in enumerate(row):
+                # Find empty and add it to the empty list
+                if cell == 0:
+                    empty.append((i, j))
+
+        # End of the game because nowhere to insert a digit
+        if not empty:
+            return False
+
+        rand_empty = random.choice(empty)
+
+        if random.random() > 0.2:
+            self.__field[rand_empty[0]][rand_empty[1]] = 2
+            self.__score = self.__score + 2
+        else:
+            self.__field[rand_empty[0]][rand_empty[1]] = 4
+            self.__score = self.__score + 4
+
+        return True
+
+    # Function for sliding the list in a certain direction and adding same close numbers
+    def __move(self, row: list, where: str) -> list:
+
+        if where in ['down', 'right']:
+            z = 2
+            while z >= 0:
+
+                if row[z] == 0 or z == 3:
+
+                    z -= 1
+
+                elif row[z] > 0:
+
+                    if row[z + 1] == 0:
+
+                        row[z + 1] = row[z]
+                        row[z] = 0
+                        z += 1
+
+                    elif row[z] == row[z + 1]:
+
+                        row[z + 1] = row[z] * 2
+                        self.__score = self.__score + row[z]
+                        row[z] = 0
+                        z += 1
+
+                    else:
+                        z -= 1
+        elif where in ['up', 'left']:
+
+            z = 1
+            while z <= 3:
+
+                if row[z] == 0 or z == 0:
+
+                    z += 1
+
+                elif row[z] > 0:
+
+                    if row[z - 1] == 0:
+
+                        row[z - 1] = row[z]
+                        row[z] = 0
+                        z -= 1
+
+                    elif row[z] == row[z - 1]:
+
+                        row[z - 1] = row[z] * 2
+                        self.__score = self.__score + row[z]
+                        row[z] = 0
+                        z -= 1
+
+                    else:
+                        z += 1
+
+        return row
+
+    # matrix transform depend on direction
+    def __transform_and_move(self, where: str) -> tuple[bool, int]:
+
+        if where in ['up', 'down']:
+
+            t_list = self.__field.T
+
+            for i, l in enumerate(t_list):
+                t_list[i] = self.__move(l, where)
+            self.__field = t_list.T
+
+        elif where in ['left', 'right']:
+
+            t_list = self.__field
+
+            for i, l in enumerate(t_list):
+                t_list[i] = self.__move(l, where)
+
+            self.__field = t_list
+        # Adding a new number
+        if not self.__add_one():
+            return self.__game_over()
+        else:
+            return True, self.__score
+
+    def __game_over(self) -> bool and int:
+        print('Game Over')
+        return False, self.__score
+
+    # Commands: Start,EndGame,Up,Down,Left,Right
+    # Returns True or False depending on the ability to add a number to game field
+    # and score
+    def input_command(self, command: str) -> tuple[bool, int]:
+
+        if self.__score == 0 and command == 'Start':
+            return self.__add_one(), self.__score
+        if self.__score != 0 and command in self.__commands:
+            return self.__transform_and_move(command)
+        if command == 'EndGame':
+            return self.__game_over()
---
+++ main.py
@@ -0,0 +1,74 @@
+from games import Game
+import sqlite3 as sq
+from datetime import datetime
+
+
+# Create a database with users and games
+def db_create():
+    with sq.connect('GU_DB.db') as con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS users
+                    (user_id INTEGER PRIMARY KEY,
+                    nickname TEXT NOT NULL UNIQUE,
+                    max_score INTEGER DEFAULT 0
+                    )""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS games
+                            (games_id INTEGER PRIMARY KEY,
+                            user_id INTEGER,
+                            score INTEGER DEFAULT 0,
+                            datetime TEXT NOT NULL
+                            )""")
+
+
+def add_user(username):
+    with sq.connect('GU_DB.db') as con:
+        cur = con.cursor()
+        cur.execute(f"SELECT nickname FROM users")
+        exist = False
+
+        for u in cur.fetchall():
+            if u[0] == username:
+                exist = True
+
+        if not exist:
+            cur.execute(f"""INSERT INTO users(nickname) VALUES ('{username}')
+            """)
+
+        cur.execute(f"SELECT user_id FROM users WHERE nickname == '{username}'")
+        user = cur.fetchone()[0]
+    return int(user)
+
+
+def add_game(score: int, user_id: int, game_time: str):
+    with sq.connect('GU_DB.db') as con:
+        cur = con.cursor()
+        cur.execute(f"""INSERT INTO games(user_id, score, datetime) 
+                    VALUES ({user_id},{score},'{game_time}')""")
+
+
+def write_max_score(user_id):
+    with sq.connect('GU_DB.db') as con:
+        cur = con.cursor()
+        cur.execute(f"""UPDATE users SET max_score = (SELECT MAX(score) 
+        FROM games WHERE user_id == {user_id})
+        WHERE user_id == {user_id}""")
+
+
+if __name__ == '__main__':
+    db_create()
+    print("Welcome to the game\n Please enter your nickname:")
+    nick = input()
+    user_cur_id = add_user(nick)
+    gm = Game()
+    print('If you want to start enter "Start" if not enter "EndGame"')
+    game_con = True
+    cur_score = 0
+    while game_con:
+        command = input('Your command:')
+        game_con, cur_score = gm.input_command(command)
+        print(gm)
+        print(f'Score:{cur_score}')
+    cur_datetime = datetime.strftime(datetime.now(), '%m/%d/%y %H:%M:%S')
+    add_game(cur_score, user_cur_id, cur_datetime)
+    print('Thanks for a game')
+    write_max_score(user_cur_id)
